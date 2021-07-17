0cfc568bddadcb9549c1cc112b7e6eb6520b7893
Arcaytus <manu.martinezgalan@gmail.com>
2021-07-17 18:46:40 -0300

    Use of the object 'props'

--- my-package/src/index.jsx
+++ my-package/src/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// ahora nuestro componente usa el objeto `props`
+// y una propiedad definida dentro que se llama `value`
+function Title(props) {
+  return <h1>{props.value}</h1>;
+} 
 
 function App() {
-  return <p>Hola</p>;
-} 
+   // Uso del componente Title con el valor "Hola Mundo!"
+  return (
+  <>
+  <Title value="Hola mundo!" />
+  <Title value="Hello world!" />)
+  </>
+  );
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
