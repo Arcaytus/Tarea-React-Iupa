121616b90bcf03b5f8434283fd1f8d0346808d62
Arcaytus <manu.martinezgalan@gmail.com>
2021-07-17 19:33:33 -0300

    Usage of React Component ".children "

--- my-package/src/index.jsx
+++ my-package/src/index.jsx
@@ -1,19 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// ahora nuestro componente usa el objeto `props`
-// y una propiedad definida dentro que se llama `value`
+
+
+function ArticleContainer(props) {
+  return <div>
+    {props.children}</div>
+}
+
 function Title(props) {
-  return <h1>{props.value}</h1>;
+  return <h1>{props.children}</h1>;
 } 
+
+function Paragraph(props) {
+  return <p>
+    {props.children}</p>
+}
+
+function EnNegrita(props) {
+  return <p>{props.children}</p>
+}
+
+function DeColorRojo(props) {
+  return <p style= {{fontStyle: 'italic', color: 'red'}} >
+    {props.children}</p>
+}
 
 function App() {
    // Uso del componente Title con el valor "Hola Mundo!"
+   // Uso de los componentes 'ArticleContainer', 'Paragraph', 'EnNegrita', 'DeColorRojo'
   return (
-  <>
-  <Title value="Hola mundo!" />
-  <Title value="Hello world!" />)
-  </>
+  <ArticleContainer>
+    <Title>
+      Hola <i>mundo</i>! 
+    </Title>
+    <Paragraph>
+        Párrafo de mi articulo
+        <EnNegrita>
+          <DeColorRojo>Importante!</DeColorRojo>
+        </EnNegrita>
+      </Paragraph>
+    </ArticleContainer>
   );
 }
 
